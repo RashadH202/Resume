a70cf12d7824e6fed00147ee4a9d3b35868f9b48
Rashad <rashad.hammonds0113@gmail.com>
2022-09-29 19:06:27 -0400

    first commit 2.0

---
+++ client/resume/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Headerphoto from './componets/headerphoto';
+
+function App() {
+  return (
+    <div className="App">
+      <Headerphoto/>
+</div>
+  );
+}
+
+export default App;
---
+++ client/resume/src/componets/headerphoto.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import materialize from 'materialize-css'
+
+export default class headerphoto extends Component {
+  render() {
+    return (
+      <div>headerphoto</div>
+    )
+  }
+}
