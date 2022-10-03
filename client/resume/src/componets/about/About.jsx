dea577049f288df5328dfa327f40d995e92965f6
Rashadtheone <rashad.hammonds0114@gmail.com>
2022-10-03 15:18:02 -0400

    resume and eduction added.

--- client/resume/src/componets/about/About.jsx
+++ client/resume/src/componets/about/About.jsx
@@ -1,8 +1,78 @@
 import React from 'react'
+import './about.css'
+import Me from "../../assets/avatar-1.svg"
+import Aboutbox from './Aboutbox'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='about container section' id ='about'>
+      <h2 className='section__title'>About Me</h2>
+      <div className='about__container grid'>
+        <img src={Me} alt='' className='about__img'/>
+            <div className="about__data grid">
+                <div className="about__info">
+                  <p className="about__decription">dfadf</p>
+                  <a href='' className='btn'>Download CV</a>
+
+                </div>
+
+                <div className='about__skills grid'>
+                  <div className="skills__data">
+                    <div className="skills__titles">
+                      <h3 className='skills__name'>Front-End Web Development</h3>
+                      <span className='skills__number'>90%</span>
+                    </div>
+
+                    <div className='skills__bar'>
+                        <span className='skills__precentage fdevelopment'>
+
+                        </span>
+                    </div>
+                  </div>
+
+                  <div className="skills__data">
+                    <div className="skills__titles">
+                      <h3 className='skills__name'>Back-End Web Development</h3>
+                      <span className='skills__number'>80%</span>
+                    </div>
+
+                    <div className='skills__bar'>
+                        <span className='skills__precentage bdevelopment'>
+
+                        </span>
+                    </div>
+                  </div>
+
+                  <div className="skills__data">
+                    <div className="skills__titles">
+                      <h3 className='skills__name'>Infrastructure Tools</h3>
+                      <span className='skills__number'>50%</span>
+                    </div>
+
+                    <div className='skills__bar'>
+                        <span className='skills__precentage infrastructure'>
+
+                        </span>
+                    </div>
+                  </div>
+                  <div className="skills__data">
+                    <div className="skills__titles">
+                      <h3 className='skills__name'>Amazon Web Services</h3>
+                      <span className='skills__number'>50%</span>
+                    </div>
+
+                    <div className='skills__bar'>
+                        <span className='skills__precentage aws'>
+
+                        </span>
+                    </div>
+                  </div>
+                </div>
+            </div>
+      </div>
+
+    <Aboutbox />
+    </section>
   )
 }
 
---
+++ client/resume/src/componets/resume/Data.jsx
@@ -0,0 +1,52 @@
+const Data = [
+    {
+        id: 0,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2013-2014",
+        title: "Year Up",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        id: 1,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2013-2014",
+        title: "NOVA Community College",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        id: 2,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2013-2014",
+        title: "General Assembly",
+        desc: "pLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        id: 3,
+        category: "experience",
+        icon: "icon-briefcase",
+        year: "2013-2014",
+        title: "CETAT",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        id: 4,
+        category: "experience",
+        icon: "icon-briefcase",
+        year: "2013-2014",
+        title: "Year Up",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        id: 5,
+        category: "experience",
+        icon: "icon-briefcase",
+        year: "2013-2014",
+        title: "Business Analyst",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+]
+
+export default Data
